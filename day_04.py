dfbcbf172aa6f435bcd81d26f5b955b0b66bf13a
Victor Chavanne <victor.chavanne@tutanota.com>
2022-12-04 20:54:57 +0100

    Setup day 04

---
+++ day_04.py
@@ -0,0 +1,26 @@
+"""
+This is Victor Chavanne's answers to day 4 of advent of code 2022 puzzles.
+"""
+
+
+def puzzle_one():
+    """This method finds the sum of the priorities of misplaced items.
+
+    :return: The sum of priorities.
+    :rtype: int
+    """
+    pass
+
+
+def puzzle_two():
+    """This method finds the sum of the priorities of badge items.
+
+    :return: The sum of priorities.
+    :rtype: int
+    """
+    pass
+
+
+if __name__ == "__main__":
+    print(f"Answer to first puzzle is : {puzzle_one()}")
+    print(f"Answer to second puzzle is : {puzzle_two()}")

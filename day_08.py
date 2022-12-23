1525422d8b307c278b97c93c5a1c8ee4ed46fd0c
Victor Chavanne <vchavanne@sophysa.fr>
2022-12-23 11:46:34 +0100

    Solved day 08

--- day_08.py
+++ day_08.py
@@ -2,7 +2,6 @@
 This is Victor Chavanne's answers to day 8 of advent of code 2022 puzzles.
 """
 
-import copy
 
 def puzzle_one():
     """This method returns the numer of visible trees.
@@ -20,50 +19,79 @@ def puzzle_one():
                 tree_line.append(int(char))
             canopy.append(tree_line)
 
-    visible_canopy = copy.deepcopy(canopy)
+    visible_canopy = [[False for _ in x] for x in canopy]
 
     # Left
     for i, line in enumerate(canopy):
         visible_tree = -1
-        for j, char in enumerate(line):
-            if char > visible_tree:
-                visible_tree = char
+        for j in range(len(line)):
+            if canopy[i][j] > visible_tree:
+                visible_tree = canopy[i][j]
                 visible_canopy[i][j] = True
-            elif visible_canopy[i][j] is not True:
-                visible_canopy[i][j] = False
-
     # Right
-    for i, line in enumerate(canopy):
         visible_tree = -1
-        for j in range(len(line)-1, -1, -1):
+        for j in reversed(range(len(line))):
             if line[j] > visible_tree:
                 visible_tree = line[j]
                 visible_canopy[i][j] = True
-            elif visible_canopy[i][j] is not True:
-                visible_canopy[i][j] = False
 
     # Top
     for j in range(len(canopy[0])):
         visible_tree = -1
-        for i in range(len(canopy)):
-            if char > visible_tree:
-                visible_tree = char
+        for i, char in enumerate(canopy):
+            if canopy[i][j] > visible_tree:
+                visible_tree = canopy[i][j]
                 visible_canopy[i][j] = True
-            elif visible_canopy[i][j] is not True:
-                visible_canopy[i][j] = False
-
     # Bottom
-    for j in range(len(canopy[0])):
         visible_tree = -1
-        for i in range(len(canopy)-1, -1, -1):
-            if char > visible_tree:
-                visible_tree = char
+        for i in reversed(range(len(canopy))):
+            if canopy[i][j] > visible_tree:
+                visible_tree = canopy[i][j]
                 visible_canopy[i][j] = True
-            elif visible_canopy[i][j] is not True:
-                visible_canopy[i][j] = False
 
     # Sum
-    result = sum([sum(x) for x in visible_canopy])
+    result_list = [sum(x) for x in visible_canopy]
+    return sum(result_list)
+
+
+def get_tree_score(canopy, col, row):
+    """This method returns the score of visible tree for one tree.
+
+    :return: The visible tree score.
+    :rtype: int
+    """
+
+    result = 1
+    height = canopy[col][row]
+
+    # Left
+    visibles = 0
+    for i in range(col-1, -1, -1):
+        visibles += 1
+        if canopy[i][row] >= height:
+            break
+    result *= visibles
+    # Right
+    visibles = 0
+    for i in range(col+1, len(canopy[0])):
+        visibles += 1
+        if canopy[i][row] >= height:
+            break
+    result *= visibles
+    # Top
+    visibles = 0
+    for j in range(row-1, -1, -1):
+        visibles += 1
+        if canopy[col][j] >= height:
+            break
+    result *= visibles
+    # Bottom
+    visibles = 0
+    for j in range(row+1, len(canopy)):
+        visibles += 1
+        if canopy[col][j] >= height:
+            break
+    result *= visibles
 
     return result
 
@@ -75,8 +103,23 @@ def puzzle_two():
     :rtype: int
     """
 
-    result = 0
-    return result
+    canopy = []
+
+    with open("inputs/day_08", "r", encoding="utf_8") as input_file:
+        for line in input_file:
+            tree_line = []
+            for char in line.strip("\n"):
+                tree_line.append(int(char))
+            canopy.append(tree_line)
+
+    score_canopy = [[0 for _ in x] for x in canopy]
+
+    for i, line in enumerate(canopy):
+        for j, _ in enumerate(line):
+            score_canopy[i][j] = get_tree_score(canopy, i, j)
+
+    result_list = [max(x) for x in score_canopy]
+    return max(result_list)
 
 
 if __name__ == "__main__":
